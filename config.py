f15d8930784c9f6c6c031673387f16313e110565
Rohan <139545262+ForgottenRohan@users.noreply.github.com>
2023-12-28 16:17:56 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
-token = 'vk1.a.yQ7HRyc0xUvTPYZzWNsCXo7PyXajweMXwZGDyCIomg4AhiV_nCMmazSXeeBNenFgz3EqnNqHLA9g9gg1AB5LeouyeTQxTFOHJh5JOUoZ1kAGtXK5qZekhAvwlFxCZngwjW7p7-9NgyB7Ogp_fteXzdKmTt3Rn_WMqqf_z67uuFAieIF4sHmUKInSqnSX7tbkqB7fdHs8-NKk33o762-ANA'
-bot_token = '6719929278:AAGFu5Ei8IIp-mEmRIltL1GEAIs7K2zW7zo'
-url_count = 5
+token = 'your vk api token'
+bot_token = 'your telegram bot token'
+url_count = int(Your count)
 groups = [
-    'soobshestvo_mrazey',
-
+    'group1',
+    'group2',
     ]
 
 admins = [
-    1400424377,
+    admin1,
+    admin2,
 ]
 channels = [
-    '@felial_vaiba',
+    '@channel1',
+    '@channel2',
 ]
 statuses = False
